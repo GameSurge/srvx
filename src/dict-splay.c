bfe215c2fe8eb715e4e7b929aa13d87066143776
Entrope <entrope@clan-dk.org>
2004-03-10 19:04:59 +0000

    License update
    * Revert to plain GPLv2.
    git-archimport-id: srvx@srvx.net--2004-srvx/srvx--devo--1.3--patch-26

--- src/dict-splay.c
+++ src/dict-splay.c
@@ -1,19 +1,17 @@
 /* dict-splay.c - Abstract dictionary type
  * Copyright 2000-2004 srvx Development Team
  *
- * This program is free software; you can redistribute it and/or modify
+ * This file is part of srvx.
+ *
+ * srvx is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.  Important limitations are
- * listed in the COPYING file that accompanies this software.
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, email srvx-maintainers@srvx.net.
  */
 
 #include "common.h"
