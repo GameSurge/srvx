b8da9873c3897e930c79f31a7a9bc089b9446a73
Michael Poole <mdpoole@troilus.org>
2007-03-11 11:18:25 -0400

    src/mail-sendmail.c: Move #include "mail-common.c" up a few lines.

--- src/mail-sendmail.c
+++ src/mail-sendmail.c
@@ -18,11 +18,11 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  */
 
+#include "mail-common.c"
+
 #ifdef HAVE_SYS_WAIT_H
 #include <sys/wait.h>
 #endif
-
-#include "mail-common.c"
 
 /* This function sends the given "paragraph" as flowed text, as
  * defined in RFC 2646.  It lets us only worry about line wrapping
