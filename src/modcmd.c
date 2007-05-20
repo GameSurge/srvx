8f5141d8da271446da910d9c7a5d20ac4a8ebd9a
ThiefMaster <thiefmaster@gamesurge.net>
2007-05-21 00:12:42 +0200

    We have year 2007 in !version, too...

--- src/modcmd.c
+++ src/modcmd.c
@@ -1,5 +1,5 @@
 /* modcmd.c - Generalized module command support
- * Copyright 2002-2006 srvx Development Team
+ * Copyright 2002-2007 srvx Development Team
  *
  * This file is part of srvx.
  *
@@ -1855,7 +1855,7 @@ static MODCMD_FUNC(cmd_version) {
      * command or its accessibility to normal IRC users, except to add
      * copyright information pertaining to changes you make to srvx.
      */
-    send_message_type(4, user, cmd->parent->bot, "$b"PACKAGE_STRING"$b ("CODENAME"), Built: "__DATE__", "__TIME__".  Copyright 2000-2006 srvx Development Team.");
+    send_message_type(4, user, cmd->parent->bot, "$b"PACKAGE_STRING"$b ("CODENAME"), Built: "__DATE__", "__TIME__".  Copyright 2000-2007 srvx Development Team.");
     if (argc > 1)
         send_message_type(4, user, cmd->parent->bot, "%s", GIT_VERSION);
     else
